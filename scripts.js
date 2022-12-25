35b17f0ffd4bdbc5680ec6e84a7f3f190bf4e2b6
user <jeffdplante@aol.com>
2022-12-25 15:00:46 -0800

    Extend the Game and Board modules

--- scripts.js
+++ scripts.js
@@ -1,32 +1,66 @@
-const gameBoard = (function(){
-  let board = [[null,null, null],[null,null, null],[null,null, null]];
+const spots = document.querySelectorAll(".spot");
 
-  function showPiece(x, y) {
-    return board[x][y];
+const gameBoard = (function(){
+  let board = ["", "", "", "", "", "", "", "", ""];
+
+  function showPiece(n) {
+    return board[n];
   }
 
-  function placePiece(piece, x, y) {
-    board[x][y] = piece; 
+  function placePiece(piece, n) {
+    board[n] = piece; 
   }
 
   function resetBoard(){
-    for (i = 0; i < 3; i++) {
-     for (j = 0; j < 3; j++) {
-       placePiece(null, i, j);
-     }
+    for (i = 0; i < board.length; i++) {
+       placePiece("", i);
     }
   }
+
   function printBoard() {
-    board.forEach((arr) => console.log(arr))
+    console.log(board)
   }
 
+  function renderBoard () {
+    for (i = 0; i < board.length; i++) {
+      spots[i].innerHTML = board[i]; 
+    }
+  }
+  
+  function checkVictory(){
+    let victoryConditions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+
+    for (i = 0; i < victoryConditions.length; i++) {
+      let first = showPiece(victoryConditions[i][0]);
+      let second = showPiece(victoryConditions[i][1]);
+      let third = showPiece(victoryConditions[i][2]);
+      
+      if (first !== "" && first === second && first === third) {
+        return true;
+      }
+    }
+    return false; 
+  }
+  
   return {
+    checkVictory,
+    renderBoard,
     showPiece,
     placePiece,
     resetBoard,
     printBoard
   }
 })();
+
+/***************************************************TESTING******************************/
+// gameBoard.printBoard()
+// console.log(gameBoard.checkVictory())
+// gameBoard.placePiece("X",2)
+// gameBoard.placePiece("X",4)
+// gameBoard.placePiece("X",6)
+// gameBoard.printBoard()
+// console.log(gameBoard.checkVictory())
+/***************************************************TESTING******************************/
 
 const player = (name, piece) => {
   function choose(coord){
@@ -44,7 +78,62 @@ const playGame = (function(){
 
 })();
 
-const game = (function(player1, player1Mark, player2, player2Mark){
-  
+const game = (function(){
 
+  let player1 = "",
+      player1Mark = "",
+      player2 = "",
+      player2Mark = "";
+
+  function newGame(p1, p1Mark, p2, p2Mark){
+    player1 = p1;
+    player1Mark = p1Mark;
+    player2 = p2;
+    player2Mark = p2Mark
+    turn.currentPlayer = p1;
+    turn.currentMark = p1Mark;
+
+    gameBoard.resetBoard();
+  }
+
+  turn = {
+    'currentPlayer' : player1,
+    'currentMark' : player1Mark,
+    'gameComplete' : false
+  }
+ 
+  function togglePlayer() {
+    if(turn.currentPlayer === player1) {
+       turn.currentPlayer = player2;
+       turn.currentMark = player2Mark;
+    } else {
+      turn.currentPlayer = player1;
+      turn.currentMark = player1Mark;
+      }
+    }
+
+  function takeTurn(n) {
+  
+    gameBoard.placePiece(turn.currentMark, n);
+  
+    gameBoard.renderBoard()
+
+    if (gameBoard.checkVictory()){
+      console.log('YOU WIN!');
+      turn.gameComplete = true;
+    }
+    
+    togglePlayer();
+  }
+
+  return {
+    newGame,
+    turn, /*********TESTING----REMOVE*****/
+    togglePlayer,  /*********TESTING----REMOVE*****/
+    takeTurn
+  }
 })();
+
+/***************************************************TESTING******************************/
+game.newGame('Jeff', 'X', 'Tom', 'O');
+/***************************************************TESTING******************************/
